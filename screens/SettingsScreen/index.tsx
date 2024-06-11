566c01cebf70159fa328e915644d8cc77293d714
Cristian Manolescu <cristimanolescu@icloud.com>
2024-06-11 12:46:48 +0300

    Chore: Settings screen partially done

--- screens/SettingsScreen/index.tsx
+++ screens/SettingsScreen/index.tsx
@@ -1,18 +1,25 @@
-import React, { useContext } from "react";
-import { View } from "react-native";
-import Text from "../../components/Text";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { AuthContext } from "../../api/auth/AuthProvider";
-import Button from "../../components/Button";
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
+import { View } from "react-native";
+import React, { useContext } from "react";
+import { useTranslation } from "react-i18next";
+
+import { makeStyles } from "@rneui/themed";
+
+import { translations } from "../../locales/translations";
 import { SettingsNavigatorParamList } from "../../navigation/AppStack/params";
+import { AuthContext } from "../../api/auth/AuthProvider";
+import Button from "../../components/Button";
+import Text from "../../components/Text";
 
 export interface SettingsPageProps
   extends StackNavigationProp<SettingsNavigatorParamList, "Settings"> {}
 
 const SettingsScreen = () => {
   const { navigate } = useNavigation<SettingsPageProps>();
+  const { t } = useTranslation();
+  const styles = useStyles();
 
   const { logout } = useContext(AuthContext);
   const handleLogout = async () => {
@@ -24,12 +31,31 @@ const SettingsScreen = () => {
   };
 
   return (
-    <SafeAreaView>
-      <Text kind="paragraph" text="SettingsScreen" />
-      <Button onPress={handleLogout} />
-      <Button onPress={handleChangeAddress} text="Change Address" />
+    <SafeAreaView style={styles.safeAreaView}>
+      <View style={styles.container}>
+        <Text kind="header" text={t(translations.settings.title)} />
+        <Button onPress={handleChangeAddress} text="Change Address" />
+      </View>
+      <View style={styles.bottomArea}>
+        <Button
+          kind="primary"
+          text={t(translations.settings.signout)}
+          onPress={handleLogout}
+        />
+        <Text kind="paragraph" text={t(translations.settings.version)} />
+      </View>
     </SafeAreaView>
   );
 };
+
+const useStyles = makeStyles(() => ({
+  safeAreaView: {
+    flex: 1,
+    gap: 20,
+    paddingHorizontal: 20,
+  },
+  container: { paddingTop: 20, gap: 20 },
+  bottomArea: { flex: 1, alignItems: "center", gap: 20 },
+}));
 
 export default SettingsScreen;
