274597677b717c4baaf364867b84201cb58e0eee
Cristian Manolescu <cristimanolescu@icloud.com>
2024-06-14 11:43:23 +0300

    Chore: Reworked book component - still in progress

--- components/Book/index.tsx
+++ components/Book/index.tsx
@@ -1,49 +1,83 @@
 import React from "react";
 import { View, TouchableOpacity } from "react-native";
+import { CheckBox } from "@rneui/themed";
+import { useEffect, useState } from "react";
+import { TouchableOpacityProps } from "react-native";
 
 import { makeStyles } from "@rneui/themed";
 
-import CheckBox from "../CheckBox";
 import Text from "../Text";
 import Icon from "../Icon";
 
-interface BookType {
-  kind: "current" | "past" | "search" | "remove";
+interface BookProps extends TouchableOpacityProps {
+  author: string;
+  title: string;
+  kind?: "library" | "search" | "order";
+  isChecked?: boolean;
+  defaultValue?: boolean;
+  onChange?: (value: boolean) => void;
 }
 
-const Book = ({ kind }: BookType) => {
+const Book = ({
+  author,
+  title,
+  kind,
+  isChecked,
+  defaultValue = false,
+  ...props
+}: BookProps) => {
   const styles = useStyles();
+
+  const [checked, setChecked] = useState<boolean | undefined>(
+    isChecked || defaultValue
+  );
+
+  useEffect(() => {
+    setChecked(isChecked);
+  }, [isChecked]);
+
+  const handlePressCheck = () => {
+    if (props.disabled) return;
+
+    const newValue = !checked;
+    setChecked(newValue);
+    props.onChange?.(newValue);
+  };
 
   const handlePress = () => {
     //TODO: Navigates to book information
   };
   return (
-    <View style={styles.container}>
-      {(kind === "current" || kind === "past") && <CheckBox />}
-      <TouchableOpacity
-        onPress={handlePress}
-        style={styles.interactiveContainer}
-      >
-        <View style={styles.texts}>
-          <Text kind="paragraph" text="Title" />
-          <Text kind="author" text="Author" />
-        </View>
-        {kind !== "remove" && <Icon icon="right" />}
-      </TouchableOpacity>
-    </View>
+    <TouchableOpacity style={styles.container}>
+      {kind !== "order" && (
+        <CheckBox
+          wrapperStyle={styles.wrapper}
+          checked={checked}
+          onPress={handlePressCheck}
+          {...props}
+          title={
+            <View style={styles.content}>
+              <View>
+                <Text text={title} kind="paragraph" />
+                <Text text={author} kind="author" />
+              </View>
+              <Icon icon="right" />
+            </View>
+          }
+        />
+      )}
+    </TouchableOpacity>
   );
 };
 
 const useStyles = makeStyles(() => ({
   container: {
+    minWidth: "100%",
+  },
+  wrapper: { gap: 20 },
+  content: {
     flexDirection: "row",
     alignItems: "center",
-  },
-  texts: {
-    flexDirection: "column",
-  },
-  interactiveContainer: {
-    flexDirection: "row",
   },
 }));
 
