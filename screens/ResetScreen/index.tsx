165734b8c8f691535d467e3e78ffb918baad293d
Cristian Manolescu <cristimanolescu@icloud.com>
2024-07-16 15:56:20 +0300

    Feat: Add Reset Password last part

--- screens/ResetScreen/index.tsx
+++ screens/ResetScreen/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useTranslation } from "react-i18next";
 
@@ -11,21 +11,50 @@ import Input from "../../components/Input";
 import Button from "../../components/Button";
 
 const ResetScreen = () => {
+  const [isReseted, setIsReseted] = useState(false);
+
   const styles = useStyles();
   const { t } = useTranslation();
+
+  const handleReset = () => {
+    setIsReseted(true);
+  };
+
+  const handleDone = () => {};
   return (
     <SafeAreaView style={styles.safeAreaView}>
       <View style={styles.container}>
         <Text text={t(translations.reset.title)} kind="header" />
-        <Text text={t(translations.reset.enterPassword)} kind="paragraph" />
-        <Input placeholder={t(translations.reset.placeholder1)} />
-        <Text text={t(translations.reset.newPassword)} kind="paragraph" />
-        <Input placeholder={t(translations.reset.placeholder2)} />
-        <Text text={t(translations.reset.confirmPassword)} kind="paragraph" />
-        <Input placeholder={t(translations.reset.placeholder2)} />
+        {!isReseted ? (
+          <>
+            <Text text={t(translations.reset.enterPassword)} kind="paragraph" />
+            <Input placeholder={t(translations.reset.placeholder1)} />
+            <Text text={t(translations.reset.newPassword)} kind="paragraph" />
+            <Input placeholder={t(translations.reset.placeholder2)} />
+            <Text
+              text={t(translations.reset.confirmPassword)}
+              kind="paragraph"
+            />
+            <Input placeholder={t(translations.reset.placeholder2)} />
+          </>
+        ) : (
+          <Text text={t(translations.reset.allDone)} kind="paragraph" />
+        )}
       </View>
       <View style={styles.bottomArea}>
-        <Button text={t(translations.reset.button)} kind="primary" />
+        {!isReseted ? (
+          <Button
+            text={t(translations.reset.button)}
+            kind="primary"
+            onPress={handleReset}
+          />
+        ) : (
+          <Button
+            text={t(translations.reset.done)}
+            kind="primary"
+            onPress={handleDone}
+          />
+        )}
       </View>
     </SafeAreaView>
   );
