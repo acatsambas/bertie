417a3fd88a1cd4662eb0beda609ba0f7ebe0d90e
Cristian Manolescu <cristimanolescu@icloud.com>
2024-07-16 15:51:49 +0300

    Feat: Add Delete account last part

--- screens/DeleteScreen/index.tsx
+++ screens/DeleteScreen/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useTranslation } from "react-i18next";
 
@@ -11,18 +11,44 @@ import Input from "../../components/Input";
 import Button from "../../components/Button";
 
 const DeleteScreen = () => {
+  const [isDeleted, setIsDeleted] = useState(false);
+
   const styles = useStyles();
   const { t } = useTranslation();
+
+  const handleDelete = () => {
+    setIsDeleted(true);
+  };
+
+  const handleDone = () => {};
   return (
     <SafeAreaView style={styles.safeAreaView}>
       <View style={styles.container}>
         <Text text={t(translations.delete.title)} kind="header" />
-        <Text text={t(translations.delete.paragraph1)} kind="paragraph" />
-        <Text text={t(translations.delete.paragraph2)} kind="paragraph" />
-        <Input placeholder="begone!" />
+        {!isDeleted ? (
+          <>
+            <Text text={t(translations.delete.paragraph1)} kind="paragraph" />
+            <Text text={t(translations.delete.paragraph2)} kind="paragraph" />
+            <Input placeholder="begone!" />
+          </>
+        ) : (
+          <Text text={t(translations.delete.finalMessage)} kind="paragraph" />
+        )}
       </View>
       <View style={styles.bottomArea}>
-        <Button text={t(translations.delete.button)} kind="primary" />
+        {!isDeleted ? (
+          <Button
+            text={t(translations.delete.button)}
+            kind="primary"
+            onPress={handleDelete}
+          />
+        ) : (
+          <Button
+            text={t(translations.delete.done)}
+            kind="primary"
+            onPress={handleDone}
+          />
+        )}
       </View>
     </SafeAreaView>
   );
