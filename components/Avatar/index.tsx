0c5df9eaf9c5951a85da510f02d5b1be32e1f9b1
Cristian Manolescu <cristimanolescu@icloud.com>
2024-07-18 17:41:47 +0300

    Removed TODO

--- components/Avatar/index.tsx
+++ components/Avatar/index.tsx
@@ -6,8 +6,6 @@ interface AvatarProps {
   onPress?(): void;
 }
 
-//TODO: LOGIC if user have photo setup then it will show it else will show First Letters from first and last name
-
 const Avatar = ({ onPress }: AvatarProps) => {
   const { user } = useContext(AuthContext);
 
