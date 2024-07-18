3f56bcd91e08c3b30a6770f2b8c8a7496848bfb1
Cristian Manolescu <cristimanolescu@icloud.com>
2024-07-18 17:38:39 +0300

    Feat: Avatar logic

--- components/Avatar/index.tsx
+++ components/Avatar/index.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { Avatar as RNEAvatar } from "@rneui/themed";
+import { AuthContext } from "../../api/auth/AuthProvider";
 
 interface AvatarProps {
   onPress?(): void;
@@ -8,9 +9,21 @@ interface AvatarProps {
 //TODO: LOGIC if user have photo setup then it will show it else will show First Letters from first and last name
 
 const Avatar = ({ onPress }: AvatarProps) => {
+  const { user } = useContext(AuthContext);
+
+  const initials =
+    user.displayName &&
+    user.displayName
+      .match(/(\b\S)?/g)
+      .join("")
+      .toUpperCase();
+
+  const avatarURL = user.photoURL && user.photoURL;
+
   return (
     <RNEAvatar
-      title="CM"
+      title={!avatarURL && initials}
+      source={avatarURL && { uri: avatarURL }}
       rounded
       size={44}
       containerStyle={{ backgroundColor: "blue" }}
