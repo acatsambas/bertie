776143a1344ce9ae0e31f83e1967496119c66a6c
Cristian Manolescu <cristimanolescu@icloud.com>
2024-07-23 19:20:49 +0300

    Feat: User Address - read/write into database

--- screens/AddressScreen/index.tsx
+++ screens/AddressScreen/index.tsx
@@ -1,6 +1,8 @@
+import { useContext, useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useTranslation } from "react-i18next";
 import { View } from "react-native";
+import firestore from "@react-native-firebase/firestore";
 
 import { makeStyles } from "@rneui/themed";
 
@@ -8,10 +10,65 @@ import { translations } from "../../locales/translations";
 import Text from "../../components/Text";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
+import { AuthContext } from "../../api/auth/AuthProvider";
 
 const AddressScreen = () => {
+  const [addr1, setAddr1] = useState("");
+  const [addr2, setAddr2] = useState("");
+  const [city, setCity] = useState("");
+  const [postcode, setPostcode] = useState("");
+  const [country, setCountry] = useState("");
+
+  const { user } = useContext(AuthContext);
+  const userId = user.uid;
+
+  useEffect(() => {
+    fetchAddress();
+  }, []);
+
+  const fetchAddress = async () => {
+    const userAddress = await firestore()
+      .collection("Address")
+      .doc(userId)
+      .get();
+    setAddr1(userAddress.data().addr1);
+    setAddr2(userAddress.data().addr2);
+    setCity(userAddress.data().city);
+    setPostcode(userAddress.data().postcode);
+    setCountry(userAddress.data().country);
+  };
+
   const styles = useStyles();
   const { t } = useTranslation();
+
+  const handleAddr1 = (value: string) => {
+    setAddr1(value);
+  };
+  const handleAddr2 = (value: string) => {
+    setAddr2(value);
+  };
+  const handleCity = (value: string) => {
+    setCity(value);
+  };
+  const handlePostcode = (value: string) => {
+    setPostcode(value);
+  };
+  const handleCountry = (value: string) => {
+    setCountry(value);
+  };
+  const handleSave = async () => {
+    try {
+      await firestore().collection("Address").doc(userId).set({
+        addr1: addr1,
+        addr2: addr2,
+        city: city,
+        postcode: postcode,
+        country: country,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <SafeAreaView style={styles.safeAreaView}>
@@ -22,15 +79,39 @@ const AddressScreen = () => {
           kind="paragraph"
         />
         <View>
-          <Input placeholder={t(translations.settings.address.addr1)} />
-          <Input placeholder={t(translations.settings.address.addr2)} />
-          <Input placeholder={t(translations.settings.address.city)} />
-          <Input placeholder={t(translations.settings.address.postcode)} />
-          <Input placeholder={t(translations.settings.address.country)} />
+          <Input
+            placeholder={t(translations.settings.address.addr1)}
+            onChangeText={handleAddr1}
+            value={addr1}
+          />
+          <Input
+            placeholder={t(translations.settings.address.addr2)}
+            onChangeText={handleAddr2}
+            value={addr2}
+          />
+          <Input
+            placeholder={t(translations.settings.address.city)}
+            onChangeText={handleCity}
+            value={city}
+          />
+          <Input
+            placeholder={t(translations.settings.address.postcode)}
+            onChangeText={handlePostcode}
+            value={postcode}
+          />
+          <Input
+            placeholder={t(translations.settings.address.country)}
+            onChangeText={handleCountry}
+            value={country}
+          />
         </View>
       </View>
       <View style={styles.bottomArea}>
-        <Button kind="primary" text={t(translations.settings.address.button)} />
+        <Button
+          kind="primary"
+          text={t(translations.settings.address.button)}
+          onPress={handleSave}
+        />
       </View>
     </SafeAreaView>
   );
