907609ecea4a9cf7a276208de0cab8cf62f8f5ae
Cristian Manolescu <cristimanolescu@icloud.com>
2024-07-31 09:50:29 +0300

    Feat: Initiate Firestore

--- screens/SearchBookScreen/index.tsx
+++ screens/SearchBookScreen/index.tsx
@@ -3,6 +3,7 @@ import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useTranslation } from "react-i18next";
+import firestore from "@react-native-firebase/firestore";
 
 import { makeStyles } from "@rneui/themed";
 
