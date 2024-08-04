2fae3da2370dc5f24a2185df6e49ca028c330ddb
Cristian Manolescu <cristimanolescu@icloud.com>
2024-08-04 17:35:51 +0300

    Feat: Bookshops data is taken from Firestore database

--- components/BookshopsList/index.tsx
+++ components/BookshopsList/index.tsx
@@ -1,29 +1,54 @@
+import { useEffect, useState } from "react";
 import { View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
+import firestore from "@react-native-firebase/firestore";
 
-import BookShop from "../Bookshop";
 import { DiscoverNavigatorParamList } from "../../navigation/AppStack/params";
+import BookShop from "../Bookshop";
 
 export interface DiscoverPageProps
   extends StackNavigationProp<DiscoverNavigatorParamList, "Discover"> {}
 
 const BookshopsList = () => {
-  const { navigate } = useNavigation<DiscoverPageProps>();
+  const [bookshops, setBookshops] = useState([]);
+  useEffect(() => {
+    fetchBookshops();
+  }, []);
 
+  const fetchBookshops = async () => {
+    const bookShopsSnapshot = await firestore().collection("shops").get();
+    const bookShopsList = bookShopsSnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    setBookshops(bookShopsList);
+  };
+
+  const { navigate } = useNavigation<DiscoverPageProps>();
+  //
   const handleBookshop = (bookshopName: string) => {
     navigate("Bookshop", { bookshopName: bookshopName });
   };
 
   return (
     <View>
-      <BookShop
+      {bookshops.map((bookshop) => (
+        <BookShop
+          key={bookshop.id}
+          name={bookshop.name}
+          location={bookshop.city}
+          kind="default"
+          onPress={() => handleBookshop(bookshop.name)}
+        />
+      ))}
+      {/* <BookShop
         name="Daunt Books"
         location="London"
         kind="default"
         onPress={() => handleBookshop("Daunt Books")}
       />
-      <BookShop name="John Sandoe Books" location="London" kind="default" />
+      <BookShop name="John Sandoe Books" location="London" kind="default" /> */}
     </View>
   );
 };
