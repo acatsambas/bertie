7ad823f846717eaa46c1be60ddb7ce25c54b1bc6
Cristian Manolescu <cristimanolescu@icloud.com>
2024-08-19 11:37:32 +0300

    Feat: Add book from Search Books to my list toggle

--- components/SearchBooks/index.tsx
+++ components/SearchBooks/index.tsx
@@ -1,22 +1,62 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
+import firestore from "@react-native-firebase/firestore";
 
 import { LibraryNavigatorParamList } from "../../navigation/AppStack/params";
+import { AuthContext } from "../../api/auth/AuthProvider";
 import Book from "../Book";
 
 export interface SearchBookProps
   extends StackNavigationProp<LibraryNavigatorParamList, "Search"> {}
 
 const SearchBooks = ({ books }) => {
+  const [isSaved, setIsSaved] = useState(false);
   const { navigate } = useNavigation<SearchBookProps>();
+  const { user } = useContext(AuthContext);
+  const userId = user.uid;
+
   const handleBook = (id: string, title: string, author: string) => {
     navigate("Book", {
       id: id,
       bookName: title,
       author: author,
     });
+  };
+
+  const handleAddBook = async (
+    id: string,
+    title: string,
+    author: string,
+    isRead: boolean
+  ) => {
+    try {
+      !isSaved
+        ? await firestore()
+            .collection("users")
+            .doc(userId)
+            .collection("books")
+            .doc(id)
+            .set(
+              {
+                author: author,
+                bookId: id,
+                isRead: isRead,
+                title: title,
+              },
+              { merge: true }
+            )
+        : await firestore()
+            .collection("users")
+            .doc(userId)
+            .collection("books")
+            .doc(id)
+            .delete();
+      setIsSaved(!isSaved);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -45,6 +85,14 @@ const SearchBooks = ({ books }) => {
                         (index ? ", " : "") + author
                     )
                   : book.volumeInfo.authors
+              )
+            }
+            onChange={() =>
+              handleAddBook(
+                book.id,
+                book.volumeInfo.title,
+                book.volumeInfo.authors,
+                false
               )
             }
           />
