8e2b5380857b5232d7ee53299c125f322e46883f
Cristian Manolescu <cristimanolescu@icloud.com>
2024-08-19 17:32:49 +0300

    Chore: Remove inactive code

--- screens/BookScreen/index.tsx
+++ screens/BookScreen/index.tsx
@@ -3,8 +3,6 @@ import { RouteProp, useRoute } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useTranslation } from "react-i18next";
 import { View } from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import { StackNavigationProp } from "@react-navigation/stack";
 import firestore from "@react-native-firebase/firestore";
 
 import { makeStyles } from "@rneui/themed";
@@ -16,9 +14,6 @@ import Text from "../../components/Text";
 import Image from "../../components/Image";
 import Button from "../../components/Button";
 
-export interface BookPageProps
-  extends StackNavigationProp<LibraryNavigatorParamList, "Book"> {}
-
 const BookScreen = ({ navigation }) => {
   const [description, setDescription] = useState("");
   const [imageCover, setImageCover] = useState("");
@@ -27,7 +22,6 @@ const BookScreen = ({ navigation }) => {
   const { params } = useRoute<RouteProp<LibraryNavigatorParamList, "Book">>();
   const styles = useStyles();
   const { t } = useTranslation();
-  const { navigate } = useNavigation<BookPageProps>();
   const { user } = useContext(AuthContext);
   const userId = user.uid;
   const { bookName, author, id } = params;
