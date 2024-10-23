cd4f819631e49ab0003bd5ae6b67740d515e186c
Cristian Manolescu <cristimanolescu@icloud.com>
2024-10-23 09:18:43 +0300

    Chore: minimal change

--- .github/scripts/decrypt.sh
+++ .github/scripts/decrypt.sh
@@ -6,7 +6,7 @@ gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_PASSPHRASE" \
   --output android/app/google-services.json android/app/google-services.json.gpg
 
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_PASSPHRASE" \
-  --output ios/vidramusic/GoogleService-Info.plist ios/vidramusic/GoogleService-Info.plist.gpg
+  --output ios/bertie/GoogleService-Info.plist ios/bertie/GoogleService-Info.plist.gpg
 
 # Signing Keys
 
