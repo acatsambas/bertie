fbc37849605e0b29c071e27eb29c4ccb738e904c
Razvan-Gabriel Geangu <razvan.geangu@gmail.com>
2024-11-17 13:21:40 +0200

    feat: ios build fastlane

--- .github/scripts/decrypt.sh
+++ .github/scripts/decrypt.sh
@@ -13,11 +13,11 @@ gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_PASSPHRASE" \
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_PASSPHRASE" \
   --output android/app/release.keystore android/app/release.keystore.gpg
 
-# gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_PASSPHRASE" \
-#   --output fastlane/AuthKey_6C5352C8R8.p8 fastlane/AuthKey_6C5352C8R8.p8.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_PASSPHRASE" \
+  --output fastlane/AuthKey_MB44GW24AY.p8 fastlane/AuthKey_MB44GW24AY.p8.gpg
 
-# gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_PASSPHRASE" \
-#   --output fastlane/bertie-434108-5fd4928195d9.json fastlane/bertie-434108-5fd4928195d9.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_PASSPHRASE" \
+  --output fastlane/bertie-74d8c-e01b7dfc401f.json fastlane/bertie-74d8c-e01b7dfc401f.json.gpg
 
 # Env
 
--- .github/scripts/encrypt.sh
+++ .github/scripts/encrypt.sh
@@ -13,8 +13,8 @@ gpg --quiet --batch --yes --symmetric --passphrase="$SECRETS_PASSPHRASE" \
 gpg --quiet --batch --yes --symmetric --passphrase="$SECRETS_PASSPHRASE" \
   --output android/app/release.keystore.gpg android/app/release.keystore
 
-# gpg --quiet --batch --yes --symmetric --passphrase="$SECRETS_PASSPHRASE" \
-#   --output fastlane/AuthKey_6C5352C8R8.p8.gpg fastlane/AuthKey_6C5352C8R8.p8
+gpg --quiet --batch --yes --symmetric --passphrase="$SECRETS_PASSPHRASE" \
+  --output fastlane/AuthKey_MB44GW24AY.p8.gpg fastlane/AuthKey_MB44GW24AY.p8
 
 gpg --quiet --batch --yes --symmetric --passphrase="$SECRETS_PASSPHRASE" \
   --output fastlane/bertie-74d8c-e01b7dfc401f.json.gpg fastlane/bertie-74d8c-e01b7dfc401f.json
